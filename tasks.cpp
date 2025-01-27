7809bf7d124b0996f67067e6c91ef841e40cfcd0
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2025-01-27 15:07:10 +0800

    Added getMenu() function in tasks.cpp

--- tasks.cpp
+++ tasks.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include "task.h"
+#include <vector>
 
 
 using std::cout;
@@ -9,36 +10,56 @@ using std::endl;
 using std::string;
 using std::cin;
 
+void getMenu();
 
 int main() {
-    task task;
-    taskList my_task_list;
-
-    cout << "Enter task name: ";
-    getline(cin, task.task_name);
-    // Call input methods to set dates
-    task.input_start_date();
-    task.input_due_date();
-    task.task_status();
-
-    // Add the task to the task list
-    my_task_list.add_task(task);
-
-    // Convert time points to time_t for printing
-    std::time_t start_time = std::chrono::system_clock::to_time_t(task.start_date);
-    std::time_t due_time = std::chrono::system_clock::to_time_t(task.due_date);
-
-    cout << "The task is: " << task.task_name << endl;
-    cout << "Starting on: " << std::ctime(&start_time);
-    cout << "Due on: " << std::ctime(&due_time);
-    cout << task.task_status() << endl;
-
-    // Output the list of tasks
-    cout << "\nListing all tasks:\n";
-    my_task_list.list_tasks();
+    getMenu();
 
     return 0;
 };
 
+void getMenu(){
+    task task;
+    taskList my_task_list;
+
+
+    char menu;
+    cout << "1. Add Task" << endl;
+    cout << "2. View Task" << endl;
+    cout << "3. Edit Task" << endl; //include Delete task here
+    cin >> menu;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // To ignore the newline character left in the input buffer
+
+
+    if (menu == '1') { // We need to loop here, as well as show the other options, and not have it exit
+        cout << "Enter task name: ";
+        getline(cin, task.task_name);
+        // Call input methods to set dates
+        task.input_start_date();
+        task.input_due_date();
+
+        // Add the task to the task list
+        //task.push(add_task);
+
+        // Convert time points to time_t for printing
+        std::time_t start_time = std::chrono::system_clock::to_time_t(task.start_date);
+        std::time_t due_time = std::chrono::system_clock::to_time_t(task.due_date);
+        
+        cout << task.get_task_status() << endl;
+        cout << "The task is: " << task.task_name << endl;
+        cout << "Starting on: " << std::ctime(&start_time);
+        cout << "Due on: " << std::ctime(&due_time);
+        
+        my_task_list.list_tasks();
+    }
+    else if (menu == '2'){
+        // Output the list of tasks
+        cout << "\nListing all tasks:\n";
+        my_task_list.list_tasks();
+    } else {
+        cout << "Invalid choice. Please try again." << endl;
+    }
+
+}
 
 
