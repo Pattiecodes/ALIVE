d876211e06b84739b09aa4c0e4b4d0f2a6b24d14
James Patrick De Ocampo <deocampojamespatrickcode@gmail.com>
2025-04-11 19:54:15 +0800

    Fixed gitignore file, and removed unnecessary files

---
+++ DEVLOGS.md
@@ -0,0 +1,169 @@
+DEVLOGS:
+
+# Jan 26, 2025
+![Sample Output](image.png)
+## Updates 
+- task.h with the following changes:
+    - Added task status method in task class that will ask the user for current task status.
+    - Created taskList() class that will be used to store all the tasks and show it to user via a list. Uses a vector.
+    - taskList() also has an add_task method that pushes back the task to the list (vector).
+    - taskList() has a list_tasks() method that will print all tasks.
+- tasks.cpp:
+    - Added taskList constructor and a separate output method for the taskList() class from task.h file
+
+## Problems encountered:
+- Only one task can be created, and does not output more or ask the user for more tasks. (tasks.cpp)
+- task_status cannot be updated on an existing task. (task.h)
+
+## Bugs:
+- Outputs the task status on a single task, which shouldn't happen. (task.h || task.cpp)
+
+## Suggestions:
+- The task_status should also update the memory. If a task is done. It should be stored somewhere else, but not in the same taskList.
+- Maybe dissect the taskList method to two parts. The existing_taskList and the done_taskList, so as to separate the two types of tasks. Implementing a bool function may work for this.
+
+## Room for improvement:
+- Overloading (function, operator)
+- Logical Operators
+
+
+# Jan 27, 2025
+## Updates 
+- task.h with the following changes:
+    - Created a get_task_status method in task class, so task_status won't overload.
+- tasks.cpp:
+    - Added void getMenu() for a more streamlined and non-confusing navigation.
+    - getMenu() includes an Add Task method, a View Tasks method, and an Edit Task method.
+    - Resolved the bug: Outputs the task status on a single task, which shouldn't happen. (task.h || task.cpp).
+
+## Problems encountered:
+- Check with Jan 26, 2025 Problems encountered.
+- getMenu() still lacks Options 2 and 3 and a loop for menu.
+- add_task() method should be able to have an option of adding more tasks to it, than exiting immediately. (tasks.cpp && task.h)
+
+## Bugs:
+- No bugs encountered today.
+
+## Suggestions:
+- The task_status should also update the memory. If a task is done. It should be stored somewhere else, but not in the same taskList.
+- Maybe dissect the taskList method to two parts. The existing_taskList and the done_taskList, so as to separate the two types of tasks. Implementing a bool function may work for this.
+- Implement a loop for the getMenu(), as well as for the add_task() method.
+
+## Room for improvement:
+- See Jan. 26, 2025 Room for improvement. As of Jan. 27, 2025.
+
+
+# Jan 28, 2025
+## Updates 
+- task.h with the following changes:
+    - Added a check_status() class, inheriting from class Task().
+- tasks.cpp:
+    - Added the check_status() in Option 2 in getMenu().
+
+## Problems encountered:
+- N/A
+
+## Bugs:
+- Check status won't show up, in the "View tasks" option - Bug found due to 'too many function calls'. (tasks.cpp)
+- check_status() has incomplete constructor. (task.h)
+
+
+## Suggestions:
+- Same as Jan 26-27, 2025 suggestions. But also:
+    - Maybe do not loop to continually add tasks as we don't want it to loop to add tasks forever. Let it be as is. As we'll add a database here too.
+    - Clean up code for task.h file, as it is becoming messy and unreadable.
+
+## Room for improvement:
+- Overloading (function, operator)
+- Logical Operators
+- Brush up on fundamentals
+- How to create an app with c++?
+
+# Jan 28, 2025
+## Updates 
+- Added ReactJS Framework.
+
+## Problems encountered:
+- N/A
+
+## Bugs:
+- N/A
+
+## Suggestions:
+- The ReactJS framework may be removed if a better way to create the app (Web app) is found.
+- See Jan 28, 2025 update.
+
+## Room for improvement:
+- Overloading (function, operator)
+- Logical Operators
+- Brush up on fundamentals.
+- ReactJS or C# for UI, and everything else.
+
+
+# Jan 30, 2025
+## Updates 
+- task.h with the following changes:
+    - 
+- tasks.cpp:
+    - 
+
+## Problems encountered:
+- 
+
+## Bugs:
+- 
+
+## Suggestions:
+- 
+
+## Room for improvement:
+- Overloading (function, operator)
+- Logical Operators
+- Brush up on fundamentals.
+
+# Apr 10 && 11, 2025
+## Updates 
+- task.h with the following changes:
+    - 
+- tasks.cpp:
+    - 
+- General:
+    - Removed node files. They were unnecessary.
+
+## Problems encountered:
+- 
+
+## Bugs:
+- 
+
+## Suggestions:
+- 
+
+## Room for improvement:
+- Overloading (function, operator)
+- Logical Operators
+- Brush up on fundamentals.
+
+
+------------ Default ------------
+# Jan 30, 2025
+## Updates 
+- task.h with the following changes:
+    - 
+- tasks.cpp:
+    - 
+
+## Problems encountered:
+- 
+
+## Bugs:
+- 
+
+## Suggestions:
+- 
+
+## Room for improvement:
+- Overloading (function, operator)
+- Logical Operators
+- Brush up on fundamentals.
+
---
+++ task.h
@@ -0,0 +1,176 @@
+
+#ifndef TASK_H
+#define TASK_H
+    #include <iostream>
+    #include <string>
+    #include <iomanip>
+    #include <ctime>
+    #include <vector>
+    #include <chrono>
+    #include <sstream>
+    #include <vector>
+
+    using std::string;
+    using std::cout;
+    using std::endl;
+
+    // defining what a task is:
+   
+    class task {
+        public:
+        string task_name;
+        std::chrono::system_clock::time_point start_date;
+        std::chrono::system_clock::time_point due_date;
+        //the task class should define what a task is:
+        // 1. a task should be able to be assigned a priority
+        // 2. a task should be able to be assigned a deadline
+        // 3. a task should be able to be assigned a status
+        // 4. a task should be able to be asssigned a category
+        // 5. a task should be able to be assigned a description
+        // 6. a task should be able to be assigned a date
+        // 7. a task should be able to have a 'gantt chart' like representation
+            // 7.a a task should be able to be assigned a time
+        
+        //task() constructor
+        task() : task_name(""), start_date(), due_date() {}
+
+        //set_start_date method, with date string - a passed by reference value 
+        void set_start_date(const string& date_str) {
+            start_date = parse_date(date_str);
+        }
+
+        void input_start_date() {
+            string date_str;
+            cout << "Enter start date (YYYY-MM-DD HH:MM:SS): ";
+            std::getline(std::cin, date_str);
+            return set_start_date(date_str);
+        }
+
+        void set_due_date(const string& date_str) {
+            due_date = parse_date(date_str);
+        }
+
+        void input_due_date() {
+            string date_str;
+            cout << "Enter due date (YYYY-MM-DD HH:MM:SS): ";
+            std::getline(std::cin, date_str);
+            return set_due_date(date_str);
+            
+        }
+
+        int task_status() {
+            int status;
+            status = get_task_status();
+            return status;
+        }
+        int get_task_status() {
+            enum task_status {not_started, in_progress, done};
+           cout << "Enter task status (0 for not started, 1 for in progress, 2 for done): ";
+           int task_status_progress;
+           std::cin >> task_status_progress;
+           if (task_status_progress == 0) {
+             cout << "Task status: Not Started" << std::endl;}
+            else if(task_status_progress == 1) {
+                cout << "Task status: In Progress" << std::endl;
+            }
+            else {
+                cout << "Task status: Done" << std::endl;
+            }
+
+            return task_status_progress;
+        }
+
+
+        private:
+            /**
+             * @brief Parses a date string and converts it to a std::chrono::system_clock::time_point.
+             *
+             * This function takes a date string in the format "%Y-%m-%d %H:%M:%S" and converts it
+             * to a std::chrono::system_clock::time_point. If the date string cannot be parsed,
+             * it throws a std::runtime_error.
+             *
+             * @param date_str The date string to be parsed.
+             * @return A std::chrono::system_clock::time_point representing the parsed date and time.
+             * @throws std::runtime_error If the date string cannot be parsed.
+             */
+            std::chrono::system_clock::time_point parse_date(const string& date_str) {
+            std::tm tm = {};
+            std::istringstream ss(date_str);
+            ss >> std::get_time(&tm, "%Y-%m-%d %H:%M:%S");
+            if (ss.fail()) {
+                throw std::runtime_error("Failed to parse date");
+            }
+            std::time_t time = std::mktime(&tm);
+            return std::chrono::system_clock::from_time_t(time);
+        }
+
+        
+    };
+
+
+//taskList class for task class.
+class taskList {
+    public:
+
+    taskList() {}
+    
+    void add_task(const task& t) {
+        /**
+     *
+     * This function iterates over the collection of tasks and prints the name of each task.
+     * 
+     * The for-loop uses a range-based for loop to iterate over each task in the `tasks` collection.
+     * - `const` ensures that the tasks are not modified during iteration.
+     * - `auto&` automatically deduces the type of each task and uses a reference to avoid copying.
+     * - `t` is the loop variable representing each task in the collection.
+     * - `: tasks` specifies that the loop should iterate over the `tasks` collection.
+     * This method iterates through the list of tasks and prints the name of each task to the standard output.
+     * 
+     * The for-loop iterates over each task in the 'tasks' container.
+     * For each task, it prints the task name using 'cout'.
+     */
+        tasks.push_back(t);
+        int tasksNumber[] = {};
+        for (const auto& t : tasks) {
+            cout << "Task Name: " << t.task_name << "\n";
+        }
+    }
+    void list_tasks() const {
+        for (const auto& t : tasks) {
+            // Print the name of each task
+            cout << "Task Name: " << t.task_name << "\n";
+
+        }  
+    }
+    private:
+    std::vector<task> tasks;
+
+};
+
+// check_status class from task class to update and simplify updating the status of a task.
+// this should be updated toon when we view the "view tasks" or "edit tasks" option.
+class check_status : public task {
+    public:
+    void setUpdate_task_status(int status) {
+        if (task_status() == 2) {
+            cout << task_status() << endl;
+        } else {
+            while (task_status() != 2 || task_status() < 0 || task_status() < 1 || task_status() > 2) {
+                cout << "Enter task status (0 for not started, 1 for in progress, 2 for done):" << endl;
+                get_task_status();
+                break;
+            }
+            cout << "Invalid value" << endl;
+        }
+    }
+
+    check_status() {}
+    private:
+    int get_task_status();
+    int task_status();
+    int set_due_date();
+    int set_start_date();
+};
+#endif // TASK_H
+
+// Work omitting the taskList class to add a task to the list and view the tasks.
